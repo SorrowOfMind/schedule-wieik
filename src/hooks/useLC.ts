020bc3a1f72bfd5baedfc7a45525d31f74806813
SorrowOfMind <sortoflifecraze@gmail.com>
2023-07-15 17:59:44 +0200

    local storage hook

---
+++ src/hooks/useLC.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const useLC = <T>(key: string, initialValue: T | (() => T)) => {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key);
+
+    if (jsonValue !== null) {
+      return JSON.parse(jsonValue);
+    }
+
+    if (initialValue instanceof Function) {
+      return initialValue();
+    }
+    return initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLC;
